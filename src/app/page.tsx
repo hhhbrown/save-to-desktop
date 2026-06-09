276b0fb48d98b1d8535c1039b4dca14b3cdb5201
Hannah Brown <hmbrown@students.cs.ubc.ca>
2026-06-09 13:27:26 -0700

    Added background options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <main className="relative min-h-screen overflow-hidden text-[#34213f]">
             <div
                 className="absolute inset-0 bg-cover bg-center"
-                style={{ backgroundImage: "url('/background.jpg')" }}
+                style={{ backgroundImage: "url('/background6.jpg')" }}
             />
             <div className="absolute inset-0 bg-white/40" />
 
