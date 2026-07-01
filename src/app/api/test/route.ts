8adc6595f4f37626d19f7639ca91d51e83aa6280
Hannah Brown <hmbrown@students.cs.ubc.ca>
2026-07-01 11:33:51 -0700

    Added s3 lib and delete test folder

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json({
-        region: process.env.AWS_REGION,
-        table: process.env.DYNAMODB_FOLDERS_TABLE,
-    });
-}
